a4a22ae38378290cccf87db3b9bd0c14d2761c25
sealey-crypto <sealey@gmail.com>
2026-01-15 15:55:02 -0500

    Update nhl_shots.py

--- nhl_shots.py
+++ nhl_shots.py
@@ -86,9 +86,6 @@ def last10(player_id):
     # Need at least 5 games to be meaningful
     return shots if len(shots) >= 5 else None
 
-if name == "Jack Quinn":
-    print("DEBUG Quinn last10 shots:", shotsN)
-
 def stddev(vals):
     m = sum(vals) / len(vals)
     return sqrt(sum((v - m) ** 2 for v in vals) / len(vals))
